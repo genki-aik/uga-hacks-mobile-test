4f3be1e43a8c372962d9e0972cb9ca822d0bf926
Genki Aikawa <ga07600@uga.edu>
2023-01-21 15:17:38 -0500

    Added auth context for mobile

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,38 +1,23 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import {
-    GoogleAuthProvider,
-    getAuth,
-    signInWithPopup,
-    onAuthStateChanged,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    sendEmailVerification,
-    sendPasswordResetEmail,
-  } from "firebase/auth";
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 import {
-    doc,
-    getFirestore,
-    query,
-    getDocs,
-    setDoc,
-    collection,
-    where,
-    addDoc,
-    updateDoc,
-    serverTimestamp,
-    limit,
-    getDoc,
-    FirestoreError
+  doc,
+  setDoc,
+  collection,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
 } from "firebase/firestore";
 import { auth, db } from "../config/firebase";
-// import { Events } from "../enums/events";
-// import { Users } from "../enums/userType";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { FirebaseError } from "firebase/app";
-
-//import { RegisterForm } from "../interfaces/registerForm";
-//import { useRouter } from "next/router";
 
 export interface UserType {
   email: string | null;
@@ -55,27 +40,34 @@ const AuthContext = createContext({});
 
 export const useAuth = () => useContext<any>(AuthContext);
 
-export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const [user, setUser] = useState<UserType>({ email: null, uid: null });
   const [userInfo, setUserInfo] = useState<UserInfoType>({
     first_name: null,
     last_name: null,
     points: 0,
     registered: {
-      "HACKS8": null
+      HACKS8: null,
     },
     //user_type: null
-  })
+  });
   const [loading, setLoading] = useState<boolean>(true);
-  const [firstName, setFirstName] = useState<string>("");
-  const [lastName, setLastName] = useState<string>("");
-  const [resumeLink, setResumeLink] = useState<string>("");
-  // const [currEvent, setCurrEvent] = useState<Events>();
 
+  // Stage Environment
+  const userRefStage = collection(db, "users-stage");
+  const eSportsRefStage = collection(db, "user-e-sports-details-stage");
+  const registerRefStage = collection(db, "user-registration-details-stage");
+  const workshopRefStage = collection(db, "user-workshop-details-stage");
+
+  // Prod Environment
   const userRef = collection(db, "users");
-  const registrationRef = collection(db, "registration");
-
-  // const router = useRouter()
+  const eSportsRef = collection(db, "user-e-sports-details");
+  const registerRef = collection(db, "user-registration-details");
+  const workshopRef = collection(db, "user-workshop-details");
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (curr_user) => {
@@ -98,201 +90,136 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
 
   const validUser = () => {
     if (user) {
-        return true;
+      return true;
     }
 
     return false;
+  };
+
+  function getFirstAndLastNameFromGoogleName(
+    full_name: string | null
+  ): [string, string] {
+    // if name does not exist
+    if (!full_name) {
+      return ["", ""];
+    }
+
+    let first_name, last_name, rest;
+    [first_name, last_name, ...rest] = full_name.split(" ");
+
+    return [first_name, last_name];
   }
 
-  // const storeUserRegistrationInformation = async (data: RegisterForm) => {
-  //   const storage = getStorage();
-  //   const file = data.resume[0]
-  //   console.log(file.name)
-  //   const storageRef = ref(storage, 'resume/' + user.uid + '/' + file.name)
-
-  //   const uploadTask = uploadBytesResumable(storageRef, file)
-
-  //   uploadTask.on('state_changed',
-  //     (snapshot) => {
-  //       console.log("upload in progress")
-  //     },
-  //     (error) => {
-  //       console.log("Error uploading resume")
-  //       alert(error)
-  //     },
-  //     async () => {
-  //       await getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-  //         setDoc(doc(db, "registration", user.uid ? user.uid : ""), {
-  //           uid: user.uid,
-  //           firstName: data.firstName,
-  //           lastName: data.lastName,
-  //           gender: data.gender,
-  //           phoneNumber: data.phoneNumber,
-  //           countryResidence: data.countryResidence.label,
-  //           year: data.year,
-  //           major: data.major,
-  //           inputMajor: data.inputMajor,
-  //           minor: data.minor,
-  //           school: data.school.value,
-  //           inputSchool: data.inputSchool,
-  //           email: data.email,
-  //           participated: data.participated,
-  //           hopeToSee: data.hopeToSee,
-  //           dietaryRestrictions: data.dietaryRestrictions,
-  //           shirtSize: data.shirtSize,
-  //           codeOfConduct: data.codeOfConduct,
-  //           eventLogisticsInfo: data.eventLogisticsInfo,
-  //           mlhCommunication: data.mlhCommunication,
-  //           resumeLink: downloadURL,
-  //           submitted_time: serverTimestamp(),
-  //         });
-  //       })
-  //     }
-  //   )
-          
-  //   // Set the user status to registered for hacks8
-  //   await updateDoc(doc(userRef, user.uid ? user.uid : ""), {
-  //     "registered.HACKS8": true
-  //   })
-
-  // }
-
-  const signUp = async (first_name: string, last_name: string, email: string, password: string) => {
+  const signUp = async (
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string
+  ) => {
     try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-        setFirstName(first_name)
-        setLastName(last_name)
-        const user = res.user;
-        const name = first_name + " " + last_name;
-        await setDoc(doc(userRef, user.uid), {
-            uid: user.uid,
-            first_name: first_name,
-            last_name: last_name,
-            name: name,
-            authProvider: "local",
-            email: email,
-            points: 0,
-            registered: {},
-            added_time: serverTimestamp(),
-        });
-        sendEmailVerification(user)
-        signOut(auth)
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
+      const user = res.user;
+      const name = first_name + " " + last_name;
+
+      await setDoc(doc(userRef, user.uid), {
+        uid: user.uid,
+        first_name: first_name,
+        last_name: last_name,
+        name: name,
+        authProvider: "local",
+        email: email,
+        points: 0,
+        registered: {},
+        added_time: serverTimestamp(),
+      });
+      sendEmailVerification(user);
+      signOut(auth);
     } catch (err: any) {
       throw err;
-      
-      // if (err instanceof FirebaseError) {
-      //   console.log(err.code)
-      //   console.log(err.name)
-      //   if (err.code == "auth/email-already-in-use") {
-      //     alert("This email is already registered with us, please login using that email")
-      //     router.push('/login')
-      //   } else if (err.code == "auth/weak-password") {
-      //     // at least 6 characters long
-      //   }
-      // }
-      //   console.error(err);
     }
   };
 
   const logIn = async (email: string, password: string) => {
-    alert("LOGGIN IN")
     const res = await signInWithEmailAndPassword(auth, email, password);
     const user = res.user;
 
-    console.log("Check for email verification")
     if (!user.emailVerified) {
-      console.log("Not verified")
-      setUser({ uid: null, email: null})
-      signOut(auth)
-      return false
+      setUser({ uid: null, email: null });
+      signOut(auth);
+      return false;
     }
-    
-    return true
+
+    return true;
   };
 
   const resetPassword = async (email: string) => {
-    await sendPasswordResetEmail(auth, email)
-  }
+    await sendPasswordResetEmail(auth, email);
+  };
 
   const logInWithGoogle = async () => {
     try {
-        const res = await signInWithPopup(auth, googleProvider);
-        const google_user = res.user;
-        // const q = query(collection(db, "users"), where("uid", "==", user.uid));
-        // const docs = await getDocs(q);
+      const res = await signInWithPopup(auth, googleProvider);
+      const google_user = res.user;
+      // const q = query(collection(db, "users"), where("uid", "==", user.uid));
+      // const docs = await getDocs(q);
 
-        const docRef = doc(db, "users", google_user.uid);
-        const docSnap = await getDoc(docRef);
-
-        if (!docSnap.exists()) {
-            await setDoc(doc(userRef, google_user.uid), {
-                uid: google_user.uid,
-                first_name: "",
-                last_name: "",
-                name: google_user.displayName,
-                authProvider: "google",
-                email: google_user.email,
-                points: 0,
-                registered: {
-                  'HACKS8': true,
-                },
-                added_time: serverTimestamp(),
-            });
-        }
-    } catch (err: any) {
-        console.error(err);
-    }
-  };
-
-  const storeFirstAndLastName = async (first_name: string, last_name: string) => {
-    try {
-        const docRef = doc(db, "users", user.uid ? user.uid : "");
-        await updateDoc(docRef, {
-            first_name: first_name,
-            last_name: last_name,
-        });
-        setFirstName(first_name)
-        setLastName(last_name)
-    } catch (err: any) {
-        console.log(err);
-    }
-  };
-
-  const hasFirstAndLastName = async () => {
-      console.log("HAS FIRST AND LAST?");
-      console.log(user);
-      const docRef = doc(db, "users", user.uid ? user.uid : "1");
+      const docRef = doc(userRef, google_user.uid);
       const docSnap = await getDoc(docRef);
-      console.log(docSnap.data());
-      console.log(docSnap.exists() ? docSnap.data().first_name : null);
-      console.log("CHECK");
-      if (docSnap.exists()) {
-        console.log(docSnap.data().first_name === "");
-        console.log(docSnap.data().first_name)
-      }
-      if (docSnap.exists() && (docSnap.data().first_name === "" || docSnap.data().last_name === "")) {
-        console.log("ENTERED THE IF STATEMENT");
-          return false;
-      }
 
-      return true;
+      const [first_name, last_name] = getFirstAndLastNameFromGoogleName(
+        google_user.displayName
+      );
+
+      if (!docSnap.exists()) {
+        await setDoc(doc(userRef, google_user.uid), {
+          uid: google_user.uid,
+          first_name: first_name,
+          last_name: last_name,
+          name: google_user.displayName,
+          authProvider: "google",
+          email: google_user.email,
+          points: 0,
+          registered: {},
+          added_time: serverTimestamp(),
+        });
+      }
+      setUserInformation(google_user.uid);
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  const storeFirstAndLastName = async (
+    first_name: string,
+    last_name: string
+  ) => {
+    try {
+      const docRef = doc(userRef, user.uid ? user.uid : "");
+
+      await updateDoc(docRef, {
+        first_name: first_name,
+        last_name: last_name,
+      });
+      setUserInformation(user.uid);
+    } catch (err: any) {
+      console.log(err);
+    }
   };
 
   const getFirstName = async () => {
-    const docRef = doc(db, "users", user.uid ? user.uid : "0");
+    const docRef = doc(userRef, user.uid ? user.uid : "0");
     const docSnap = await getDoc(docRef);
 
     if (!docSnap.exists()) {
       return null;
     }
-    console.log("DOCSNAP")
-    console.log(docSnap.data())
+
     return docSnap.data().first_name;
-  }
+  };
 
   const getRegisteredEvents = async () => {
-    const docRef = doc(db, "users", user.uid ? user.uid : "0");
+    const docRef = doc(userRef, user.uid ? user.uid : "0");
     const docSnap = await getDoc(docRef);
 
     if (!docSnap.exists()) {
@@ -300,11 +227,10 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
     }
 
     return docSnap.data().registered;
-  }
+  };
 
   const setUserInformation = async (uid: string | null) => {
-    console.log("Setting user information");
-    const docRef = doc(db, "users", uid ? uid : "");
+    const docRef = doc(userRef, uid ? uid : "");
     const docSnap = await getDoc(docRef);
 
     if (!docSnap.exists()) {
@@ -317,16 +243,31 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
       points: docSnap.data().points,
       registered: docSnap.data().registered,
       //user_type: docSnap.data().user_type,
-    })
-  }
-  
+    });
+  };
+
   const logOut = async () => {
     setUser({ email: null, uid: null });
     await signOut(auth);
   };
 
   return (
-    <AuthContext.Provider value={{ user, userInfo, signUp, logIn, resetPassword, logInWithGoogle, logOut, storeFirstAndLastName, hasFirstAndLastName, validUser, getFirstName, getRegisteredEvents, setUserInformation }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        userInfo,
+        signUp,
+        logIn,
+        resetPassword,
+        logInWithGoogle,
+        logOut,
+        storeFirstAndLastName,
+        validUser,
+        getFirstName,
+        getRegisteredEvents,
+        setUserInformation,
+      }}
+    >
       {loading ? null : children}
     </AuthContext.Provider>
   );
