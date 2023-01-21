78dff173cf53b774760032ec24f7b0505acd45fe
Genki Aikawa <ga07600@uga.edu>
2023-01-21 14:33:25 -0500

    Added header title custom component

--- App.tsx
+++ App.tsx
@@ -4,20 +4,20 @@
  *
  * @format
  */
-import 'react-native-gesture-handler';
-import React from 'react';
-import {Image, SafeAreaView, StatusBar, Text, View} from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { Image, SafeAreaView, StatusBar, Text, View } from "react-native";
 
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 // import Parse from 'parse/react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {UserRegistration} from './UserRegistration';
-import {UserLogIn} from './UserLogIn';
-import {UserLogOut} from './UserLogOut';
-import {HelloUser} from './HelloUser';
-import Styles from './Styles';
-import { AuthContextProvider } from './context/AuthContext';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { UserRegistration } from "./UserRegistration";
+import { UserLogIn } from "./UserLogIn";
+import { UserLogOut } from "./UserLogOut";
+import { HelloUser } from "./HelloUser";
+import Styles from "./Styles";
+import { AuthContextProvider } from "./context/AuthContext";
 
 // // Your Parse initialization configuration goes here
 // Parse.setAsyncStorage(AsyncStorage);
@@ -35,14 +35,12 @@ function UserRegistrationScreen() {
       <StatusBar />
       <SafeAreaView style={Styles.login_container}>
         <View style={Styles.login_header}>
-          <Image
-            style={Styles.login_header_logo}
-          />
+          <Image style={Styles.login_header_logo} />
           <Text style={Styles.login_header_text}>
             <Text style={Styles.login_header_text_bold}>
-              {'React Native on Back4App - '}
+              {"React Native on Back4App - "}
             </Text>
-            {' User registration'}
+            {" User registration"}
           </Text>
         </View>
         <UserRegistration />
@@ -57,19 +55,38 @@ function UserLogInScreen() {
       <StatusBar />
       <SafeAreaView style={Styles.login_container}>
         <View style={Styles.login_header}>
-          <Image
-            style={Styles.login_header_logo}
-          />
+          <Image style={Styles.login_header_logo} />
           <Text style={Styles.login_header_text}>
             <Text style={Styles.login_header_text_bold}>
-              {'React Native on Back4App - '}
+              {"React Native on Back4App - "}
             </Text>
-            {' User login'}
+            {" User login"}
           </Text>
         </View>
         <UserLogIn />
       </SafeAreaView>
     </AuthContextProvider>
+  );
+}
+
+function LogoTitle() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        style={{ width: 30, height: 30 }}
+        source={require("./assets/UGAHacks_General_Byte.png")}
+      />
+      <Text style={{ color: "white", padding: 5, fontSize: 22 }}>
+        UGA Hacks ByteMobile
+      </Text>
+    </View>
   );
 }
 
@@ -81,13 +98,13 @@ function HomeScreen() {
         <View style={Styles.login_header}>
           <Image
             style={Styles.login_header_logo}
-            source={require('./assets/icon-google.png')}
+            source={require("./assets/icon-google.png")}
           />
           <Text style={Styles.login_header_text}>
             <Text style={Styles.login_header_text_bold}>
-              {'React Native on Back4App - '}
+              {"React Native on Back4App - "}
             </Text>
-            {' Home'}
+            {" Home"}
           </Text>
         </View>
         <HelloUser />
@@ -104,9 +121,28 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={UserLogInScreen} />
-        <Stack.Screen name="Sign Up" component={UserRegistrationScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#DC4141",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={UserLogInScreen}
+          options={{ headerTitle: () => <LogoTitle /> }}
+        />
+        <Stack.Screen
+          name="Sign Up"
+          component={UserRegistrationScreen}
+          options={{ headerTitle: () => <LogoTitle /> }}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
